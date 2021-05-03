6d9159c54c8411fdf7552c542d174b8e8a517fb4
Erik Tollerud <erik.tollerud@gmail.com>
2021-05-03 17:49:19 -0400

    first working version

---
+++ temp_receiver.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import board
+import busio
+import digitalio
+import adafruit_rfm69
+import datetime
+
+DEFAULT_ENCRYPTION = b'radioplusTCiscoo'
+
+
+class TempReceiver:
+    """
+    Receives messages of the form "TempU:##.##,Vbat:###.##"
+    and logs them
+    """
+    def __init__(self, cspin=board.D16, resetpin=board.D13, spinum=1, freq=433.0):
+        cs = digitalio.DigitalInOut(cspin)
+        reset = digitalio.DigitalInOut(resetpin)
+        if spinum==0:
+            suffix = ''
+        else:
+            suffix = '_' + str(spinum)
+        sck = getattr(board, 'SCK' + suffix)
+        mosi = getattr(board, 'MOSI' + suffix)
+        miso = getattr(board, 'MISO' + suffix)
+        spi = busio.SPI(sck, MOSI=mosi, MISO=miso)
+
+        rfm69 = adafruit_rfm69.RFM69(spi, cs, reset, freq,
+                                     encryption_key=DEFAULT_ENCRYPTION)
+        print('rfm69 started')
+        self.rfm69 = rfm69
+
+    def get_data(self, **recv_kwargs):
+        msg = self.rfm69.receive(**recv_kwargs)
+        if msg.startswith(b'TempC:'):
+            # valid
+            floats = [float(c.split(b':')[1]) for c in msg.split(b',')]
+            return tuple(floats)
+        else:
+            raise ValueError('Did not get a valid message')
+
+    def temp_log(self, timeout=61, fnout=None, n=None):
+
+        if fnout is None:
+            f = None
+        else:
+            if not os.path.exists(fnout):
+                with open(fnout, 'w') as f:
+                    f.write('timestamp temp_c vbat\n')
+
+            f = open(fnout, 'a')
+        try:
+            i = 0
+            while n is None or i < n:
+                try:
+                    temp_c, v_bat = self.get_data(timeout=timeout)
+                    dt = datetime.datetime.now()
+
+                    line = str(dt).replace(' ', 'T')
+                    line += ' ' + str(temp_c)
+                    line += ' ' + str(v_bat)
+                    print(line)
+                    if f is not None:
+                        f.write(line)
+                        f.write('\n')
+                        f.flush()
+                    print('RSSI:', self.rfm69.last_rssi)
+                except Exception as e:
+                    print('Temp receipt failed due to', e)
+                i += 1
+        finally:
+            if f is not None:
+                f.close()
+
+
+    @staticmethod
+    def deg_c_to_f(degc):
+        return degc*1.8 + 32
+
+if __name__ == '__main__':
+    r = TempReceiver()
+
+    if len(sys.argv) == 1:
+        r.temp_log(n=5)
+    elif len(sys.argv) == 2:
+        r.temp_log(fnout=sys.argv[1])
+    else:
+        print('invalid argvs')
+        sys.exit(1)
---
+++ temp_sender.ino
@@ -0,0 +1,260 @@
+//https://github.com/milesburton/Arduino-Temperature-Control-Library
+
+
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_SleepyDog.h>
+#include <SPI.h>
+#include <RH_RF69.h>
+
+// pin 3 is SCL, 2 is SDA
+#define ONE_WIRE_BUS 3
+
+#define WAIT_MS 10000
+
+//define to use watchdox for low-power waiting
+#define SLEEP_WAIT 1
+// comment out to turn off LED
+//#define LED           13
+
+// don't define to skip reading
+#define VBATPIN A9
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+/*
+ * Setup function. Here we do the basics
+ */
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  
+  int i = 0;
+  while (!Serial && (i < 20))  {
+    // wait for serial port to connect. Needed for native USB
+    // Or if it goes for 1 sec, assume its never connecting and move on with life
+    delay(50); 
+    i++;
+  }
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  
+  // Assign address manually. The addresses below will beed to be changed
+  // to valid device addresses on your bus. Device address can be retrieved
+  // by using either oneWire.search(deviceAddress) or individually via
+  // sensors.getAddress(deviceAddress, index)
+  // Note that you will need to use your specific address here
+  //insideThermometer = { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 };
+
+  // Method 1:
+  // Search for devices on the bus and assign based on an index. Ideally,
+  // you would do this to initially discover addresses on the bus and then 
+  // use those addresses and manually assign them (see above) once you know 
+  // the devices on your bus (and assuming they don't change).
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  
+  // method 2: search()
+  // search() looks for the next device. Returns 1 if a new address has been
+  // returned. A zero might mean that the bus is shorted, there are no devices, 
+  // or you have already retrieved all of them. It might be a good idea to 
+  // check the CRC to make sure you didn't get garbage. The order is 
+  // deterministic. You will always get the same devices in the same order
+  //
+  // Must be called before search()
+  //oneWire.reset_search();
+  // assigns the first address found to insideThermometer
+  //if (!oneWire.search(insideThermometer)) Serial.println("Unable to find address for insideThermometer");
+
+  // show the addresses we found on the bus
+  Serial.print("Device 0 Address: ");
+  printAddress(insideThermometer);
+  Serial.println();
+
+  // set the resolution
+  sensors.setResolution(insideThermometer, 12);
+ 
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.println();
+
+  setup_radio();
+}
+
+// function to print the temperature for a device
+float printTemperature(DeviceAddress deviceAddress)
+{
+  // method 1 - slower
+  //Serial.print("Temp C: ");
+  //Serial.print(sensors.getTempC(deviceAddress));
+  //Serial.print(" Temp F: ");
+  //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
+
+  // method 2 - faster
+  float tempC = sensors.getTempC(deviceAddress);
+  if(tempC == DEVICE_DISCONNECTED_C) 
+  {
+    Serial.println("Error: Could not read temperature data");
+    return;
+  }
+  Serial.print("Temp C: ");
+  Serial.print(tempC);
+  Serial.print(" Temp F: ");
+  Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+
+  return tempC;
+}
+/*
+ * Main function. It will request the tempC from the sensors and display on Serial.
+ */
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  
+  // It responds almost immediately. Let's print out the data
+  sendTemperature(printTemperature(insideThermometer)); // Use a simple function to print out the data
+
+#ifdef SLEEP_WAIT
+   //requires Adafruit_SleepyDog
+   int towait = WAIT_MS;
+   while (towait > 0) {
+       towait -= Watchdog.sleep(towait);
+   }
+// USBDevice.attach();
+//   Serial.print("waited ");
+//   Serial.print(WAIT_MS - towait);
+//   Serial.println(" ms");
+#else
+  delay(WAIT_MS);
+#endif
+
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+
+
+// RADIO
+#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     7
+  #define RFM69_RST     4
+#endif
+
+#define RF69_FREQ 433.
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+void setup_radio(void)
+{
+    // Serial should have been started already
+
+    
+#ifdef LED
+  pinMode(LED, OUTPUT);     
+#endif
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+ 
+  Serial.println("Feather RFM69 RX Test!");
+  Serial.println();
+ 
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+
+  if (!rf69.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+ 
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+ 
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = { 114, 97, 100, 105, 111, 112, 108, 117, 115, 84, 67, 105, 115, 99, 111, 111};
+  rf69.setEncryptionKey(key);
+}
+
+void sendTemperature(float tempC) {
+
+ 
+  char radiopacket[30] = "TempC:###.###";
+  //sprintf (radiopacket, "TempC:%g", tempC);
+  dtostrf(tempC ,7, 3, radiopacket+6);
+  
+#ifdef VBATPIN
+  char batstr[10] = ",VBat:#.##";
+  dtostrf(get_vbat() ,4, 2, batstr+6);
+  strcat(radiopacket, batstr);
+#endif
+  
+  Serial.print("Sending:\""); Serial.print(radiopacket); Serial.println("\"");
+  
+  // Send a message!
+  rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+  rf69.waitPacketSent();
+  rf69.sleep();
+
+  
+#ifdef LED
+  digitalWrite(LED, HIGH);
+  delay(30);
+  digitalWrite(LED, LOW);
+  delay(30);
+  digitalWrite(LED, HIGH);
+  delay(30);
+  digitalWrite(LED, LOW);    
+#endif
+}
+
+
+float get_vbat() {  
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  return measuredvbat;
+}
